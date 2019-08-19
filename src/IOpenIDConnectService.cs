582b7a0ffe89f5a8e3d231b97ba4c100c2ea66e3
leonardo.formaggi <leonardo.formaggi@benner.com.br>
2019-08-19 14:52:02 -0300

    Adicionando metodos uteis para manipulacao de jwt

--- src/IOpenIDConnectService.cs
+++ src/IOpenIDConnectService.cs
@@ -1,4 +1,7 @@
-﻿namespace Benner.Tecnologia.Common.Services
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Benner.Tecnologia.Common.Services
 {
     public interface IOpenIDConnectService
     {
@@ -9,5 +12,13 @@
         dynamic GetRawJson(string accessToken);
 
         UserInfo GetUserInfo(string accessToken);
+
+        /// <summary>
+        /// Validates the token with certificate and throws exception if invalid
+        /// </summary>
+        /// <param name="accessToken"></param>
+        ClaimsPrincipal ValidateToken(string accessToken, out JwtSecurityToken jwt);
+
+        dynamic GetJsonPayloadFromToken(string accessToken);
     }
 }
